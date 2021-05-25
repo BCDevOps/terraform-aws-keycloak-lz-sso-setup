8777dc53c93f599314d9937488697555b83b0294
Warren Uniewski <warren@autimo.com>
2021-05-25 16:59:34 -0700

    allow keycloak provider versions >= 2.0.0

--- main.tf
+++ main.tf
@@ -1,43 +1,46 @@
 terraform {
-	required_providers {
-		keycloak = {
-			source = "mrparkers/keycloak"
-			version = ">= 2.0.0"
-		}
-		aws = {
-			source = "hashicorp/aws"
-			version = "3.11.0"
-		}
-	}
+  required_providers {
+    keycloak = {
+      source  = "mrparkers/keycloak"
+      version = ">= 2.0.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.11.0"
+    }
+    autimo-http = {
+      source  = "terraform.autimo.com/autimo/http"
+      version = "2.0.0"
+    }
+  }
 }
 
-provider "http" {
-	version = "2.0.0"
-}
+provider "http" {}
 
 data "keycloak_realm" "kc-lz-sso-realm" {
-	realm = var.kc_realm
+  realm = var.kc_realm
 }
 
 // Note: we need to use the http data source over the keycloak provider's `keycloak_saml_client_installation_provider` because recent versions of KeyCloak such as ours don't expose the descriptor in the same way as prior versions.
 data "http" "saml_idp_descriptor" {
-	url = "${var.kc_base_url}/auth/realms/${var.kc_realm}/protocol/saml/descriptor"
-	request_headers  = {
-		Accept = "application/xml"
-	}
+  provider = autimo-http
+  url      = "${var.kc_base_url}/auth/realms/${var.kc_realm}/protocol/saml/descriptor"
+  request_headers = {
+    Accept = "application/xml"
+  }
 }
 
 resource "aws_iam_saml_provider" "default" {
-	name                   = var.aws_saml_idp_name
-	saml_metadata_document = tostring(data.http.saml_idp_descriptor.body)
+  name                   = var.aws_saml_idp_name
+  saml_metadata_document = tostring(data.http.saml_idp_descriptor.body)
 }
 
 resource "aws_iam_role" "admin_role" {
 
-	for_each = var.account_roles
-	name = each.key
+  for_each = var.account_roles
+  name     = each.key
 
-	assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -59,32 +62,32 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "role-policy-attach" {
-	depends_on = [aws_iam_role.admin_role]
-	for_each = var.account_roles
+  depends_on = [aws_iam_role.admin_role]
+  for_each   = var.account_roles
 
-	role = each.key
-	policy_arn = each.value
+  role       = each.key
+  policy_arn = each.value
 }
 
-data aws_caller_identity "aws_context" {}
+data "aws_caller_identity" "aws_context" {}
 
 module "cloud_roles" {
 
-		source = "github.com/BCDevOps/terraform-keycloak-role-group-simplification"
+  source = "github.com/BCDevOps/terraform-keycloak-role-group-simplification"
 
-	realm = var.kc_realm
-	iam_auth_client_id = var.kc_iam_auth_client_id
+  realm              = var.kc_realm
+  iam_auth_client_id = var.kc_iam_auth_client_id
 
-	//	module operates on a list of accounts.  this allows us to define a bunch of projects and get all the accounts created for them.
-//	@todo fix this so it supports distinct values for each of hte attributes insttead of va.raccount_name for everything
-	accounts = [ {
-		project_identifier = var.account_name
-		project_name = var.account_name
-		name = var.account_name
-		environment = var.account_name
-		account_number = data.aws_caller_identity.aws_context.account_id
-	}]
-	role_names = [for role,arn in var.account_roles : role]
-	idp_name = var.aws_saml_idp_name
+  //	module operates on a list of accounts.  this allows us to define a bunch of projects and get all the accounts created for them.
+  //	@todo fix this so it supports distinct values for each of hte attributes insttead of va.raccount_name for everything
+  accounts = [{
+    project_identifier = var.account_name
+    project_name       = var.account_name
+    name               = var.account_name
+    environment        = var.account_name
+    account_number     = data.aws_caller_identity.aws_context.account_id
+  }]
+  role_names = [for role, arn in var.account_roles : role]
+  idp_name   = var.aws_saml_idp_name
 }
 
